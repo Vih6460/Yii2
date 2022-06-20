b79165414febed7e6b56fab41bdeebc5bda64158
Vinicius <viniciuz6460@gmail.com>
2022-06-20 05:05:23 -0300

    separando header e footer em arquivos diferentes e adicionando novo item na nav bar

---
+++ advanced/frontend/views/layouts/footer.php
@@ -0,0 +1,10 @@
+<?php
+use yii\bootstrap4\Html;
+?>
+
+<footer class="footer mt-auto py-3 text-muted">
+    <div class="container">
+        <p class="float-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
+        <p class="float-right"><?= Yii::powered() ?></p>
+    </div>
+</footer>
